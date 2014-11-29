5e4d96f7bd7733731485dc28ddb2f3ac6bd4976e
saeed <saeedpiri72@gmail.com>
2014-11-29 19:34:43 +0330

    Product class added

--- StoreManager/StoreModels/Product.cs
+++ StoreManager/StoreModels/Product.cs
@@ -16,51 +16,48 @@ namespace StoreManager
         private string name;
         private long sellPrice;
 
-        public long MyProperty
+        public long SellPrice//esm ha be in soorate
         {
             get { return sellPrice; }
             set { sellPrice = value; }
         }
 
-        public string MyProperty
+        public string Name
         {
             get { return name; }
             set { name = value; }
         }
 
-        public int MyProperty
+        public int Code
         {
             get { return code; }
             set { code = value; }
         }
 
-        public Category MyProperty
+        public Category Category
         {
             get { return category; }
             set { category = value; }
         }
 
-        public long MyProperty
+        public long BuyPrice
         {
             get { return buyPrice; }
             set { buyPrice = value; }
         }
 
-        public string MyProperty
+        public string BarCode
         {
             get { return  barCode; }
             set {  barCode = value; }
         }
 
-        public int MyProperty
+        public int Availability
         {
             get { return availability; }
             set { availability = value; }
         }
 
-        public int Code()
-        {
 
-        }
     }
 }
