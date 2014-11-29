6f1174c3e5e34a6fd13e887e8845c106e7f7404a
saeed <saeedpiri72@gmail.com>
2014-11-29 21:51:52 +0330

    Transaction class added

---
+++ StoreManager/StoreModels/Transaction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManager.StoreModels
+{
+    class Transaction
+    {
+        private int paymentType;
+        private int code;
+        private Contact contact;
+        private Data creationData;
+        private int paymentData;
+        private long totalPrice;
+        private int type;
+
+        public int Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        
+        public long TotalPrice
+        {
+            get { return totalPrice; }
+            set { totalPrice = value; }
+        }
+        
+        public int PaymentData
+        {
+            get { return paymentData; }
+            set { paymentData = value; }
+        }
+        
+        public Data CreationData
+        {
+            get { return creationData; }
+            set { creationData = value; }
+        }
+        
+        public Contact Contact
+        {
+            get { return contact; }
+            set { contact = value; }
+        }
+        
+        public int Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+        
+    }
+}
