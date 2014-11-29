b58a0a5fbbadd92f2e41b25cefc3e2acbf363d50
saeed <saeedpiri72@gmail.com>
2014-11-29 22:05:42 +0330

    ProductTransactions class added

---
+++ StoreManager/StoreModels/ProductTransaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManager.StoreModels
+{
+    class ProductTransaction:ProductTransactionItem
+    {
+        private ProductTransactionItem items;
+
+        public ProductTransactionItem Items
+        {
+            get { return items; }
+            set { items = value; }
+        }
+        public long TotalDiscount
+        {
+            get { return items; }
+        }
+        public long TotalPrice
+        {
+            get { return items; }
+        }
+        public long PriceAfterDiscount
+        {
+            get { return TotalPrice-TotalDiscount; }
+        }
+    }
+}
