ee217e6da4bf7e9b95cab2ab971c70f0b198cfb3
saeed <saeedpiri72@gmail.com>
2014-11-29 22:09:45 +0330

    Moneytransaction class added

---
+++ StoreManager/StoreModels/MoneyTransaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManager.StoreModels
+{
+    class MoneyTransaction:Transaction
+    {
+        private int isPersonal;
+
+        public int IsPersonal
+        {
+            get { return isPersonal; }
+            set { isPersonal = value; }
+        }
+        
+    }
+}
