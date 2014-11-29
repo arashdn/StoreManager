532b83fbef215df79ab4402e3d2e26d05760b12b
saeed <saeedpiri72@gmail.com>
2014-11-29 20:03:06 +0330

    Check class added

---
+++ StoreManager/StoreModels/Check.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreManager.StoreModels
+{
+    class Check
+    {
+        private string bankBranch;
+        private string checkNumber;
+        private int code;
+        private long price;
+        private int vosoolStatus;
+
+        public int VosoolStatus
+        {
+            get { return vosoolStatus; }
+            set { vosoolStatus = value; }
+        }
+        
+        public long Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        
+        public int Code
+        {
+            get { return code; }
+            set { code = value; }
+        }
+        
+        public string CheckNumber
+        {
+            get { return checkNumber; }
+            set { checkNumber = value; }
+        }
+
+        public string BankBranch
+        {
+            get { return bankBranch; }
+            set { bankBranch = value; }
+        }
+
+    }
+}
