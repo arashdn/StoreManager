63a177247e7495f18ad7be602cf74de631071a72
Arashdn <arash@dargahi.org>
2014-12-13 19:18:00 +0330

    new classes add

---
+++ StoreManager/Store.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoreManager.StoreModels;
+
+namespace StoreManager
+{
+    class Store
+    {
+        public void sell(ProductTransactionItem pti, DateTime date, string not = null, bool isPaid = true, Contact con = null, Check chk = null)
+        {
+            List<ProductTransactionItem> list = new List<ProductTransactionItem>();
+            list.Add(pti);
+            sell(list, date,not,isPaid,con,chk);
+        }
+
+        public void sell(List<ProductTransactionItem> pti , DateTime date,  string not = null , bool isPaid = true ,Contact con = null , Check chk = null)
+        {
+            DateTime? paydate = null;
+            if(isPaid)
+                paydate=DateTime.Now;
+            ProductTransaction pt = new ProductTransaction()
+            {
+                CreationDate = date,
+                PaymentDate = paydate,
+                Check = chk,
+                Contact = con,
+                Note=not, 
+                
+            };
+            DBContext myDBContext = new DBContext();
+            foreach (ProductTransactionItem item in pti)
+            {
+                pt.items.Add(item);
+                myDBContext.products.Attach(item.Product); //association with product and dbcontext
+            }
+            
+            myDBContext.save(pt);
+            
+        }
+    
+    
+        public void buy(ProductTransactionItem pti, DateTime date, string not = null, bool isPaid = true, Contact con = null, Check chk = null)
+        {
+
+        }
+
+        public void buy(List<ProductTransactionItem> pti , DateTime date,  string not = null , bool isPaid = true ,Contact con = null , Check chk = null)
+        {
+
+        }
+    
+    
+    }
+}
---
+++ StoreManager/Timing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoreManager.StoreModels;
+
+namespace StoreManager
+{
+    class Timing
+    {
+        public List<MoneyTransaction> getDayBedehi(DateTime date)
+        {
+            DBContext myDB = new DBContext();//We need this to use function - association
+            return null;
+        }
+
+        public List<MoneyTransaction> getTodayBedehi()
+        {
+            return getDayBedehi(DateTime.Now);
+        }
+    }
+}
